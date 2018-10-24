0fe8ff69e0119f87de4e5c435f5761be799f700c
moratorium08 <moratorium08@gmail.com>
2018-10-24 11:52:09 +0900

    Add show mandelbrot

--- asm.sh
+++ asm.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 BASE=`dirname $0`
-exec python3 $BASE/asm-lib/main.py "$@"
+exec python3 $BASE/asm-lib/asm/main.py "$@"
---
+++ scripts/show_mandelbrot.py
@@ -0,0 +1,20 @@
+import sys
+filename = 'mandelbrot'
+x = 400
+y = 400
+
+with open(filename) as f:
+    cnt = 0
+    for line in f.readlines():
+        if line[0] == '0':
+            sys.stdout.write(' ')
+        else:
+            sys.stdout.write('▉')
+        cnt += 1
+        if cnt == 400:
+            cnt = 0
+            sys.stdout.write('\n')
+
+
+
+
