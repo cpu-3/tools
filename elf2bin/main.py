68aa762e5d288a481f984bc632e817020e800390
moratorium08 <moratorium08@gmail.com>
2019-01-28 00:21:15 +0900

    Fix zero padding between sections

--- elf2bin/main.py
+++ elf2bin/main.py
@@ -12,11 +12,17 @@ binary = lief.parse(filename)
 sections = ['.reset.boot', '.text', '.rodata']
 
 pad = 0
+num_wrote = 0
 with open(output, 'wb') as f:
     for name in sections:
         section = binary.get_section(name)
         data = bytes(section.content)
+
+        zero_pad_size = section.virtual_address - num_wrote
+        f.write(b'\x00' * zero_pad_size)
+        num_wrote += zero_pad_size
         f.write(data)
         pad = (pad + section.size) % 4
+        num_wrote += section.size
 
     f.write(b'\x00' * (4 - pad))
