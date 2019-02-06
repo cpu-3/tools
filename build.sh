f080cbc175e38a92a7b82d735d30b18ce340b4fd
moratorium08 <moratorium08@gmail.com>
2019-02-06 14:31:45 +0900

    Add emu

--- build.sh
+++ build.sh
@@ -11,14 +11,19 @@ mkdir -p $BIN/asm-lib
 mkdir -p $BIN/compiler
 mkdir -p $BIN/_simu
 mkdir -p $BIN/elf2bin-lib
+mkdir -p $BIN/_emu
 
 make clean -C simu
 make -C simu
 make clean -C compiler
 make -C compiler
+make clean -C emu
+make -C emu
+
 
 
 cp simu/simu $BIN/simu
+cp emu/emu $BIN/emu
 mv compiler/min-caml $BIN
 cp mcamlc $BIN
 cp asm.sh $BIN/asm
